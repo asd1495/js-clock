09e2fc65d64427c880d7d08370a7f020a7106623
asd1495 <armando.salcedo95@gmail.com>
2022-10-28 14:50:22 -0500

    fixed hours

--- main.js
+++ main.js
@@ -2,17 +2,16 @@ const secondsHand = document.querySelector('.seconds-hand')
 const minutesHand = document.querySelector('.min-hand')
 const hoursHand = document.querySelector('.hour-hand')
 
-
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
 
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60)*6) + 90;
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 24) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60)*30) + 90;
     
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
@@ -20,3 +19,4 @@ function setDate() {
 }
 
 setInterval(setDate, 1000);
+setDate();
